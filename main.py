45ec9b2f2f5066614bc1cb42f1e7d70a53d03118
Jake <jgl2832@gmail.com>
2011-02-03 12:14:34 -0500

    added GL

--- main.py
+++ main.py
@@ -1,3 +1,65 @@
+#!/usr/bin/env python2.3
+
+import OpenGL
+import sys
+OpenGL.ERROR_ON_COPY = True
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+
+
+win_width = 100
+win_height = 100
+
+def draw():
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+	## DRAW HERE
+
+	glutSwapBuffers()
+
+def reshape(width, height):
+	w = width
+	## do nothing for now
+
+def init():
+	glEnable(GL_DEPTH_TEST)
+	glEnable(GL_NORMALIZE)
+
+def idle():
+	glutPostRedisplay()
+
+def visible(vis):
+	if vis == GLUT_VISIBLE:
+		glutIdleFunc(idle)
+	else:
+		glutIdleFunc(None)
+
+def key(k,x,y):
+	if k == GLUT_KEY_LEFT:
+		a = 1
+		##Moveleft
+	if k == GLUT_KEY_RIGHT:
+		a = 2
+		##Moveright
+	## ETC...
+
+
 
 if __name__ == '__main__':
+	glutInit(sys.argv)
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH)
+
+	glutInitWindowPosition(0,0)
+	glutInitWindowSize(win_width, win_height)
+	glutCreateWindow("The Game")
+	init()
+
+	glutDisplayFunc(draw)
+	glutSpecialFunc(key)
+	glutReshapeFunc(reshape)
+	glutVisibilityFunc(visible)
+
+	glutMainLoop()
 	print "TEST"
+
+
---
+++ person.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python2.3
+
+class person(object):
+	def __init__(self):
+		self.height = 20
+		self.width = 20
+		self.loc_x = 0
+		self.loc_y = 0
